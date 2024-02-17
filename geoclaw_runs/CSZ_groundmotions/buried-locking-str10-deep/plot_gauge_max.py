da6f7004af841df639353a5a8576a3613b3308c9
Randy LeVeque <rjl@uw.edu>
2024-02-17 09:38:30 -0800

    make_dual_fgout_animation.py and improve plot_gauge_max.py

--- geoclaw_runs/CSZ_groundmotions/buried-locking-str10-deep/plot_gauge_max.py
+++ geoclaw_runs/CSZ_groundmotions/buried-locking-str10-deep/plot_gauge_max.py
@@ -8,85 +8,108 @@ from pylab import *
 
 import clawpack.pyclaw.gauges as gauges
 from clawpack.geoclaw import topotools
-from clawpack.visclaw import geoplot
+from clawpack.visclaw import plottools, geoplot
+from clawpack.geoclaw import fgout_tools
 
 event = os.path.split(os.getcwd())[-1]
+outdir_instant = '../%s_instant/_output' % event
+#outdir_instant = None
 
-outdir = '_output'
 plotdir = '_plots'
 os.system('mkdir -p %s' % plotdir)
 
 etopo = topotools.Topography('/Users/rjl/topo/topofiles/etopo1_-163_-122_38_63.asc',3)
 
+plot_eta = True
 
 gaugenos = range(1,642,20)
-xg = []
-yg = []
-gmax = []
-for gaugeno in gaugenos:
-    gauge = gauges.GaugeSolution(gaugeno, outdir)
-    eta = gauge.q[3,:]
-    gmax.append(eta.max())
-    xg.append(gauge.location[0])
-    yg.append(gauge.location[1])
 
-gmax = array(gmax)
-xg = array(xg)
-yg = array(yg)
+def read_gauges(outdir):
+    xg = []
+    yg = []
+    gmax = []
+    for gaugeno in gaugenos:
+        gauge = gauges.GaugeSolution(gaugeno, outdir)
+        eta = gauge.q[3,:]
+        gmax.append(eta.max())
+        xg.append(gauge.location[0])
+        yg.append(gauge.location[1])
 
-figure(200, figsize=(12,7))
+    gmax = array(gmax)
+    xg = array(xg)
+    yg = array(yg)
+    return xg,yg,gmax
+
+outdir = '_output'
+xg,yg,gmax = read_gauges(outdir)
+
+figure(201, figsize=(11,8))
 clf()
-
-plot(yg, gmax, 'k-o')
-xlabel('latitude')
-ylabel('offshore wave amplitude (m)')
-grid(True)
-title('Gauge amplitudes\n%s' % event)
-fname = '%s/gauge_amplitudes.png' % plotdir
-savefig(fname)
-print('Created %s' % fname)
-
-
-figure(201, figsize=(10,8))
-clf()
-ax = axes([0.6,0.1,0.3,0.8])
+ax = axes([0.5,0.1,0.4,0.8])
 ax.contourf(etopo.X, etopo.Y, etopo.Z, [-1e4,0,1e4], 
          colors=[geoplot.blue_green,geoplot.dark_green])
 
-CS = ax.contour(etopo.X, etopo.Y, etopo.Z, [-1000,-200],
-                linestyles='-', linewidths=0.5, colors='gray')
-#clabel(CS)
 
 ax.set_aspect(1./cos(48*pi/180))
-         
 
+if plot_eta:
+    fgframeno = 17
+    # Instantiate object for reading fgout frames:
+    format = 'binary'  # format of fgout grid output
+    fgout_grid = fgout_tools.FGoutGrid(1, outdir, format)      
+    fgout1 = fgout_grid.read_frame(fgframeno)
+    tfg = fgout1.t / 60.
+    eta = ma.masked_where(fgout1.h<0.001, fgout1.eta)
+
+    eta_plot = plottools.pcolorcells(fgout1.X,fgout1.Y,eta,
+                                     cmap=geoplot.tsunami_colormap)
+    clim(-4,4)
+    cb = colorbar(eta_plot, extend='both', shrink=0.5)
+    cb.set_label('meters')
+else:
+    # plot a few contours of bathymetry:
+    CS = ax.contour(etopo.X, etopo.Y, etopo.Z, [-1000,-200],
+                    linestyles='-', linewidths=0.5, colors='gray')
+    #clabel(CS)
+    
 ax.plot(xg,yg,'ko',markersize=3)  # gauge locations
 
 xlimits = [-130,-122]
 ylimits = [40,52]
 ax.set_xlim(xlimits)
 ax.set_ylim(ylimits)
-ax.set_title('Location of gauges')
+if plot_eta:
+    ax.set_title('Tsunami at %s minutes\nand location of gauges' % tfg)
+else:
+    ax.set_title('Location of gauges')
 ax.set_xlabel('longitude')
 ax.set_ylabel('latitude')
     
 ax2 = axes([0.1,0.1,0.3,0.8])
 #ax2 = axes([0.75,0.1,0.2,0.8], sharey=ax)
+
+
+if outdir_instant is not None:
+    xgi,ygi,gmaxi = read_gauges(outdir_instant)
+    base = zeros(len(gmaxi))
+    ampl = base + gmaxi
+    ax2.plot(ampl,ygi,'r-', label='instantaneous')
+    for k in range(len(ygi)):
+        ax2.plot([base[k],ampl[k]], [ygi[k],ygi[k]], 'r-')    
+
+
 base = zeros(len(gmax))
 ampl = base + gmax
-ax2.fill_between(base,ampl,color=[.6,.6,1])
-
-
-ax2.plot(ampl,yg,'b-')
+ax2.plot(ampl,yg,'b-', label='time-dependent')
 for k in range(len(yg)):
     ax2.plot([base[k],ampl[k]], [yg[k],yg[k]], 'b-')
-        
-
+    
 ax2.set_title('maximum amplitude at gauges\n%s' % event)
 ax2.grid(True)
 ax2.set_xlim(15,0)
 ax2.set_xlabel('meters')
 ax2.set_ylim(ylimits)
+ax2.legend(loc='upper left')
 
 
 fname = '%s/map_gauge_amplitudes.png' % plotdir
