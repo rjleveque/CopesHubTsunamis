f3e9c55134cb518e12bbf4bc49d5a4f687f32324
Randy LeVeque <rjl@uw.edu>
2024-06-20 10:49:20 -0700

    modify to make .dtt3 files in vertical_displacements_sensitivity

--- dtopo/CSZ_groundmotions/make_dtopo_from_utm_grid.py
+++ dtopo/CSZ_groundmotions/make_dtopo_from_utm_grid.py
@@ -1,12 +1,22 @@
 
 from pylab import *
 import os,sys
-from clawpack.geoclaw import topotools,dtopotools,kmltools
+from clawpack.geoclaw import topotools,kmltools
 from scipy.interpolate import LinearNDInterpolator
 
-defdir = 'vertical_displacements'
+#CHT = os.environ['CHT'] # path to CopesHubTsunamis directory
+CHT = '/Users/rjl/git/CopesHubTsunamis' # or hard-wired
+sys.path.insert(0,CHT+'/common_code')  # add to search path
+import dtopotools # local version, makes smaller files
+
+#defdir = 'vertical_displacements' # first 18 events
+defdir = 'vertical_displacements_sensitivity'  # 3 new events 6/20/24
+
 #fname_orig = 'vert_displacements_all_xgrid_buried-random-str10-shallow'
-fname_orig = 'vert_displacements_all_xgrid_buried-locking-skl16-shallow'
+#fname_orig = 'vert_displacements_all_xgrid_ft-random-mur13-deep'
+#fname_orig = 'vert_displacements_all_xgrid_buried-random-mur13-deep-nosub'
+fname_orig = 'vert_displacements_all_xgrid_buried-random-mur13-deep-nosub-homogeneous-halfspace-noQ'
+
 path_orig = os.path.join(defdir, fname_orig)
 lon,lat,z = loadtxt(path_orig, skiprows=1,usecols=[1,2,3],unpack=True)
 
@@ -24,7 +34,7 @@ dtopo = dtopotools.DTopography()
 dtopo.dZ = Z
 dtopo.X = X
 dtopo.Y = Y
-dtopo.times = [0]
+dtopo.times = array([0])
 ntimes = len(dtopo.times)
 #dtopo.delta = (dx,dy)
 dZshape = (ntimes,X.shape[0],X.shape[1])
@@ -40,7 +50,34 @@ dtopo.dZ = dZ
 
 fname = fname_orig + '.dtt3'
 path_new = os.path.join(defdir, fname)
-dtopo.write(fname, 3)
-print('Created ', fname)
+dtopo.write(path_new, 3)
+print('Created ', path_new)
+
+
+if 1:
+    # Make plots and animations:
+
+
+    coast = load('/Users/rjl/git/clawpack/geoclaw/scratch/pacific_shorelines_east_4min.npy')
+    x_coast = coast[:,0] - 360.
+    y_coast = coast[:,1]
+
+
+    dZ_max = abs(dtopo.dZ).max()
+    #cmax_dZ = round(dZ_max)
+    cmax_dZ = 4. # to compare to Audrey's plot
+    print('maximum |dZ| = %.2f m, setting cmax_dZ = %.1f' % (dZ_max,cmax_dZ))
+
+    # plot at final time (only time for instant):
+    fig,ax = subplots(figsize=(6,7))
+    plot(x_coast,y_coast,'g')
+    axis([-128.5,-122,40,50])
+    t = dtopo.times.max()
+    dtopo.plot_dZ_colors(t,axes=ax,cmax_dZ=cmax_dZ,dZ_interval=100)
+    title('Final vertical displacement\n%s' % fname_orig)
+    grid(True);
+    fname = '%s/%s.png' % (defdir,fname_orig)
+    savefig(fname)
+    print('Created ',fname)
 
 
