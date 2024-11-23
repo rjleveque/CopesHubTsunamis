0b529cd35cd63dfe6c88be265dcb21719e7e5b86
Randy LeVeque <rjl@uw.edu>
2024-11-23 12:51:36 -0800

    make plot of accel in make_gnss_txt.py

--- dtopo/CSZ_groundmotions/gnss/make_gnss_txt.py
+++ dtopo/CSZ_groundmotions/gnss/make_gnss_txt.py
@@ -34,3 +34,23 @@ def make_gnss_txt_file(event,station):
     fname = outdir + event_station + '_accel.txt'
     savetxt(fname, accel, comments='', header='%i' % len(ta), fmt='%14.4f')
     print('Created ', fname)
+    
+    figure(22, figsize=(12,6))
+    clf()
+    absa = maximum(abs(Na), abs(Ea))  # maximum abs accel at each time
+    maxa = max(abs(Na).max(), abs(Ea).max())
+    i1 = where(absa > 0.1*maxa)[0].min()
+    i2 = where(absa > 0.1*maxa)[0].max()
+    tmin = floor(t[i1]/25) * 25
+    tmax = ceil(t[i2]/25) * 25
+    plot(ta, Ea, 'g', label='E accel')
+    plot(ta, Na, 'r', linewidth=0.5, label='N accel')
+    xlim(tmin,tmax)
+    grid(True)
+    xlabel('time (sec)')
+    ylabel('acceleration (m/s^2)')
+    title('Station %s acceleration\nScenario %s' % (station,event))
+    legend(loc='upper right')
+    fname = outdir + event_station + '_accel.png'
+    savefig(fname, bbox_inches='tight')
+    print('Created ', fname)
