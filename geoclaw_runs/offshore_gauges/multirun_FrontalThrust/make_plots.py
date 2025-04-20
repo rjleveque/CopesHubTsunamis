3df5585bfa870a72d5da33138b0852a5b5f1d6bb
Randy LeVeque <rjl@uw.edu>
2025-04-20 07:32:22 -0700

    offshore_gauges/multirun_FrontalThrust for 18 new events

---
+++ geoclaw_runs/offshore_gauges/multirun_FrontalThrust/make_plots.py
@@ -0,0 +1,154 @@
+"""
+Code to make plots for  multiple GeoClaw runs, with a different dtopo file
+for each.
+
+It is assumed that geoclaw has already been run using run
+
+It is assumed all parameters set in setrun are the same for all realizations,
+except for the dtopo file to use.
+
+The function make_all_cases_dtopos returns *caselist*, a list of
+dictionaries.  Each dictionary should define whatever parameters
+are needed for one case.
+
+The function run_one_case_dtopo(case) takes a dictionary *case* as input
+and does what's necessary to run GeoClaw for that one case.
+
+Before running, make sure the following are properly set:
+
+    dtopo_dir is set to the directory containing the dtopo files
+
+    runs_dir is set to the destination for _output and _plots directories
+        (possibly on a scratch disk).
+
+    dtopo_files is set to a list of dtopo files to use
+    nprocs is set to the number of geoclaw runs that can be run in parallel.
+        (each run will also use OpenMP with OMP_NUM_THREADS threads)
+
+Set dry_run = False before executing to actually run GeoClaw.
+
+"""
+
+from numpy import *
+import os,sys,glob
+
+#dry_run = True  # If True, only print out settings, do not run GeoClaw
+dry_run = False  # If True, only print out settings, do not run GeoClaw
+
+# top level directory for this project:
+root_dir = os.environ['CHT']   # assuming environment variable set
+
+location = 'offshore' # need to change module names below for other locations
+
+sys.path.insert(0, os.path.abspath('..'))
+#import plot_gauge_max_offshore as plot_gauges
+import plot_fgmax
+
+# for FrontalThrust (modify?):
+import plot_gauge_max_offshore_ft as plot_gauges
+
+#import process_fgmax_offshore as process_fgmax
+import make_fgout_animation_offshore as make_fgout_animation
+
+
+# location for big files:
+this_dir = os.getcwd()
+# Randy's laptop:
+scratch_dir = this_dir.replace('git/CopesHubTsunamis/geoclaw_runs', \
+                               'scratch/CHT_runs')
+# for hyak:
+scratch_dir = scratch_dir.replace('/mmfs1/home', '/gscratch/tsunami')
+
+# where to find output for all the runs:
+# (in a subdirectory of runs_dir named geoclaw_outputs)
+runs_dir = os.path.abspath(scratch_dir)
+#runs_dir = os.path.abspath('.')
+
+print('runs_dir = ',runs_dir)
+
+# list of events to plot:
+
+if 1:
+    # all files found
+    all_dtopo_files = glob.glob('%s/*tt3' % dtopo_dir)
+    #dtopo_files = all_dtopo_files
+    # not the instant deformation versions:
+    dtopo_files = [f for f in all_dtopo_files if 'instant' not in f]
+    dtopo_files.sort()
+
+
+
+
+if 0:
+    all_models = \
+        ['buried-locking-mur13', 'buried-locking-skl16', 'buried-locking-str10',
+         'buried-random-mur13',  'buried-random-skl16',  'buried-random-str10']
+
+    models = all_models[:3]
+    events = ['%s-deep' % model for model in models] \
+           + ['%s-middle' % model for model in models] \
+           + ['%s-shallow' % model for model in models]
+
+if 0:
+    # or specify particular events:
+    #events = ['buried-random-str10-middle','buried-random-str10-shallow']
+    #events = ['ft-random-mur13-deep']
+    #events = ['buried-random-mur13-deep-nosub_instant']
+    events = ['buried-random-mur13-deep-nosub-homogeneous-halfspace-noQ_instant',
+        'buried-random-mur13-deep_nosub_shiftdoubling_instant',
+        'buried-random-mur13-deep_updiptaper_hetero_instant',
+        'buried-random-mur13-deep_updiptaper_homogen_instant']
+    #events = events[:1]
+
+geoclaw_outputs = os.path.abspath('%s/geoclaw_outputs' % runs_dir)
+outdirs = ['%s/_output_%s' % (geoclaw_outputs, event) for event in events]
+#print('outdirs = ', outdirs)
+
+geoclaw_plots = os.path.abspath('%s/geoclaw_plots' % runs_dir)
+plotdirs = ['%s/_plots_%s' % (geoclaw_plots, event) for event in events]
+#plotdirs = [outdir.replace('output','plot') for outdir in outdirs]
+print('plotdirs = ', plotdirs)
+
+if dry_run:
+    print('DRY RUN - location = %s,  events to process:\n' % location ,events)
+
+def make_html_index(plotdir,event):
+    html_fname = os.path.join(plotdir,'index.html')
+    with open(html_fname, 'w') as f:
+        f.write('<html>\n<h1>%s</h1>\n' % event)
+        f.write('\n<ul>\n<li><a href="fgmax">fgmax plots</a>\n')
+        f.write('<li><a href="gauges">gauge plots</a>\n</ul>\n')
+    print('Created ',html_fname)
+
+
+if not dry_run:
+    for k in range(len(outdirs)):
+        outdir = outdirs[k]
+        plotdir = plotdirs[k]
+        event = events[k]
+        run_name = '%s_%s' % (location,event)
+
+        try:
+            if 1:
+                gauges_plotdir = plotdir + '/gauges'
+                #gaugenos = list(range(1,642,20)) + list(range(2000,2015))
+                gaugenos = 'all'
+                plot_gauges.make_gauge_plot(gaugenos, outdir, gauges_plotdir,
+                                            location, event)
+
+            if 1:
+                fgmax_plotdir = plotdir + '/fgmax'
+                #fg, t_hours = process_fgmax.load_fgmax(outdir)
+                #process_fgmax.make_fgmax_plots(fg, fgmax_plotdir, run_name, t_hours)
+                plot_fgmax.make_fgmax_plot(outdir, fgmax_plotdir,
+                                           location, event)
+
+            if 1:
+                make_fgout_animation.make_anim(outdir, plotdir, location, event)
+
+            if 1:
+                make_html_index(plotdir,event)
+
+        except:
+            print('*** Plotting failed for %s'  % run_name)
+            raise()
---
+++ geoclaw_runs/offshore_gauges/multirun_FrontalThrust/plot_gauge_max_offshore_ft.py
@@ -0,0 +1,167 @@
+
+import sys, os
+if 'matplotlib' not in sys.modules:
+    import matplotlib
+    matplotlib.use('Agg')  # Use an image backend
+
+from pylab import *
+
+import clawpack.pyclaw.gauges as gauges
+from clawpack.geoclaw import topotools
+from clawpack.visclaw import plottools, geoplot, gaugetools
+from clawpack.geoclaw import fgout_tools
+
+location = 'offshore'
+
+try:
+    CHT = os.environ['CHT']
+except:
+    raise Exception("*** Set CHT enviornment variable to repository top")
+
+etopo_file = os.path.join(CHT,
+                          'topo/topofiles/etopo22_15s_-137_-121_37_55.asc')
+
+etopo = topotools.Topography(etopo_file)
+
+#plot_eta = True
+plot_eta = False
+outdir_instant = None  # not set up for comparisons with instataneous dtopo
+
+def read_gauges(outdir, gaugenos='all'):
+    
+    if gaugenos == 'all':
+        setgauges = gaugetools.read_setgauges(outdir)
+        gaugenos = setgauges.gauge_numbers
+    
+    print('+++ %i gauges in %s' % (len(gaugenos),outdir))
+    xg = []
+    yg = []
+    gmax = []
+    for gaugeno in gaugenos:
+        gauge = gauges.GaugeSolution(gaugeno, outdir)
+        eta = gauge.q[3,:]
+        gmax.append(eta.max())
+        xg.append(gauge.location[0])
+        yg.append(gauge.location[1])
+
+    # sort gauges from south to north
+    isort = argsort(yg)
+    gaugenos = [gaugenos[i] for i in isort]
+    print('+++ gaugenos sorted S to N:\n  ',gaugenos)
+    gmax = array(gmax)
+    xg = array(xg)
+    yg = array(yg)
+    xg = xg[isort]
+    yg = yg[isort]
+    gmax = gmax[isort]
+
+    return xg,yg,gmax
+
+def make_gauge_plot(gaugenos, outdir, plotdir, location, event):
+    
+    os.system('mkdir -p %s' % plotdir)
+    run_name = '%s_%s' % (location,event)
+    
+    xg,yg,gmax = read_gauges(outdir, gaugenos=gaugenos)
+    
+    # save gmax for plotting comparisons:
+    d = vstack((yg,gmax)).T
+    fname = '%s/%s_gauges_max.txt' % (plotdir,run_name)
+    savetxt(fname, d, fmt='%.5f',header='latitude of gauge, max eta')
+    print('Created ',fname)
+
+    figure(201, figsize=(11,8))
+    clf()
+    ax = axes([0.5,0.1,0.4,0.8])
+    ax.contourf(etopo.X, etopo.Y, etopo.Z, [-1e4,0,1e4], 
+             colors=[geoplot.blue_green,geoplot.dark_green])
+
+
+    ax.set_aspect(1./cos(48*pi/180))
+
+    if plot_eta:
+        fgframeno = 17
+        # Instantiate object for reading fgout frames:
+        format = 'binary32'  # format of fgout grid output
+        fgout_grid = fgout_tools.FGoutGrid(1, outdir, format)      
+        try:
+            fgout_grid.read_fgout_grids_data() # required as of v5.11.0
+        except:
+            fgout_grid.read_fgout_grids_data_pre511()  # in case older
+        fgout1 = fgout_grid.read_frame(fgframeno)
+        tfg = fgout1.t / 60.
+        eta = ma.masked_where(fgout1.h<0.001, fgout1.eta)
+
+        eta_plot = plottools.pcolorcells(fgout1.X,fgout1.Y,eta,
+                                         cmap=geoplot.tsunami_colormap)
+        clim(-4,4)
+        cb = colorbar(eta_plot, extend='both', shrink=0.5)
+        cb.set_label('meters')
+    else:
+        # plot a few contours of bathymetry:
+        CS = ax.contour(etopo.X, etopo.Y, etopo.Z, [-1000,-200],
+                        linestyles='-', linewidths=0.5, colors='gray')
+        #clabel(CS)
+        
+    ax.grid(linewidth=0.3,color='w')
+    ax.plot(xg,yg,'ko',markersize=3)  # gauge locations
+
+    xlimits = [-130,-122]
+    ylimits = [40,52]
+    ax.set_xlim(xlimits)
+    ax.set_ylim(ylimits)
+    if plot_eta:
+        ax.set_title('Tsunami at %s minutes\nand location of gauges' % tfg)
+    else:
+        ax.set_title('Location of gauges')
+    ax.set_xlabel('longitude')
+    ax.set_ylabel('latitude')
+        
+    ax2 = axes([0.1,0.1,0.3,0.8])
+    #ax2 = axes([0.75,0.1,0.2,0.8], sharey=ax)
+
+    plot_line = False  # plot horiz line at each gauge amplitude
+
+    if outdir_instant is not None:
+        xgi,ygi,gmaxi = read_gauges(outdir_instant)
+        base = zeros(len(gmaxi))
+        ampl = base + gmaxi
+        ax2.plot(ampl,ygi,'r-', label='instantaneous')
+        if plot_line:
+            for k in range(len(ygi)):
+                ax2.plot([base[k],ampl[k]], [ygi[k],ygi[k]], 'r-')    
+
+
+    base = zeros(len(gmax))
+    ampl = base + gmax
+    #ax2.plot(ampl,yg,'b-', label='time-dependent')
+    ax2.plot(ampl,yg,'b-', label='instantaneous')
+    if plot_line:
+        for k in range(len(yg)):
+            ax2.plot([base[k],ampl[k]], [yg[k],yg[k]], 'b-')
+        
+    ax2.set_title('maximum amplitude at gauges\n%s' % event)
+    ax2.grid(True)
+    #ax2.set_xlim(14,0)
+    #ax2.set_xlim(24,0)  # modified for ft_ model
+    ampmax = max(ampl.max(), 14)
+    ax2.set_xlim(ampmax,0)
+    ax2.set_xlabel('meters')
+    ax2.set_ylim(ylimits)
+    ax2.legend(loc='upper left')
+
+
+    fname = '%s/%s_gauges_max.png' % (plotdir,run_name)
+    savefig(fname)
+    print('Created %s' % fname)
+    
+        
+if __name__ == '__main__':
+    
+    event = os.path.split(os.getcwd())[-1]
+
+    outdir = os.path.abspath('./_output')
+    plotdir = os.path.abspath('./_plots')
+
+    gaugenos = 'all'
+    make_gauge_plot(gaugenos, outdir,plotdir,location,event)
