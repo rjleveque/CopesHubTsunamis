a03f47f1faf7677f4dcd69791c63c41edbf2c712
Randy LeVeque <rjl@uw.edu>
2025-06-01 13:17:39 -0700

    Seaside/buried-random-mur13-deep from 11/17/24 comparing to Yong with different manning

---
+++ geoclaw_runs/sites/Seaside/buried-random-mur13-deep/params.py
@@ -0,0 +1,13 @@
+
+import os
+
+location = 'Seaside'  # for naming plots and mp4 file
+
+rundir = os.getcwd()
+print('rundir = %s' % rundir)
+
+# for new ground motions:
+event = os.path.split(os.getcwd())[-1]
+instant = ('instant' in rundir)  # is this instantaneous uplift?
+
+print('event = %s, instant = %s' % (event,instant))
