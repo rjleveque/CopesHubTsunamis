c2b937cbb4056c3d967561d2745d6ac35a61c5ef
Randy LeVeque <rjl@uw.edu>
2025-06-03 20:58:08 -0700

    make_allgauges_nc.py contains read_allgauges_nc, using xarray

---
+++ user_tools/make_allgauges_nc.py
@@ -0,0 +1,184 @@
+
+from pylab import *
+import numpy
+import clawpack.pyclaw.gauges as gauges
+import clawpack.amrclaw.data as amrclaw
+
+from scipy.interpolate import interp1d
+import os
+import netCDF4
+import xarray
+import time as time_module
+
+from CHTtools import event_map
+
+dry_run = False
+
+drytol = 1e-3  # set u=v=0 where h < drytol when computing from hu,hv
+
+datatype = 'f4'  # 'f4' for 4-byte float32, 'f8' for 8-byte float64
+
+
+outdirs = '/Users/rjl/git/CopesHubTsunamis/geoclaw_runs/sites/Seaside/multirun2/geoclaw_outputs'
+
+location = 'Seaside'
+
+nc_file = 'allgauges_%s.nc' % location
+print('nc_file = ',nc_file)
+
+# make a netcdf file for this one event with these gauges:
+gaugenos = range(1001,1068,1)
+
+# events:
+ievents = [1]
+event_names = [event_map[j] for j in ievents]
+
+# quantities of interest:
+gauge_qois = ['h','u','v','eta']
+iqois = range(len(gauge_qois))  # integer indices 0,1,2,3
+
+# times:
+t0 = 0.
+tfinal = 5400.
+dt = 5.
+tg = arange(t0, tfinal+.1*dt, dt)
+
+gauge_results = empty((len(tg),len(gaugenos),len(iqois),len(ievents)),
+                      dtype=float)
+
+xg = nan*zeros(len(gaugenos))
+yg = nan*zeros(len(gaugenos))
+
+tmin = inf
+tmax = -inf
+
+if dry_run:
+    print('dry_run: will check that gauge results exist for these events:')
+    print(event_names)
+
+for k,ievent in enumerate(ievents):
+    event_name = event_map[ievent]
+    outdir = '%s/_output_%s'  % (outdirs,event_name)
+
+    for j,gaugeno in enumerate(gaugenos):
+        gauge = gauges.GaugeSolution(gaugeno, outdir)
+        t = gauge.t
+        tmin = min(tmin, t.min())
+        tmax = max(tmax, t.max())
+
+        x,y = gauge.location
+        if isnan(xg[j]):
+            # first event, set x,y for this gauge:
+            xg[j] = x
+            yg[j] = y
+        if x != xg[j] or y != yg[j]:
+            print('*** x or y do not match for gaugeno %i, event %s' \
+                    % (gaugeno,event_name))
+            print('*** x,y = %g,%g, expected %g,%g' \
+                    % (x,y,xg[j],yg[j]))
+
+        if dry_run:
+            break
+
+        # set the data:
+        h = gauge.q[0,:]
+        gaugefcn = interp1d(t, h, kind='linear', bounds_error=False)
+        gauge_results[:,j,0,k] = gaugefcn(tg)
+
+        u = numpy.divide(gauge.q[1,:], h,
+                          out=numpy.zeros(h.shape, dtype=float), \
+                          where=(h>drytol))
+        gaugefcn = interp1d(t, u, kind='linear', bounds_error=False)
+        gauge_results[:,j,1,k] = gaugefcn(tg)
+
+        v = numpy.divide(gauge.q[2,:], h,
+                          out=numpy.zeros(h.shape, dtype=float), \
+                          where=(h>drytol))
+        gaugefcn = interp1d(t, v, kind='linear', bounds_error=False)
+        gauge_results[:,j,2,k] = gaugefcn(tg)
+
+        eta = gauge.q[-1,:]
+        gaugefcn = interp1d(t, eta, kind='linear', bounds_error=False)
+        gauge_results[:,j,3,k] = gaugefcn(tg)
+
+    if dry_run:
+        break
+
+print('minimum time found at any gauge = %.2f seconds' % tmin)
+print('maximum time found at any gauge = %.2f seconds' % tmax)
+
+if dry_run:
+    print('Rerun with dry_run==False to make netCDF file')
+
+if not dry_run:
+    with netCDF4.Dataset(nc_file, 'w') as rootgrp:
+
+        rootgrp.description = 'All gauge output for %s'  % location
+
+        rootgrp.history = 'Created ' + time_module.ctime(time_module.time())
+        rootgrp.outdir = os.path.abspath(outdir)
+        #rootgrp.run_finished = run_finished
+
+        #gauge = rootgrp.createGroup(gauge_name)
+        time = rootgrp.createDimension('time', len(tg))
+        gaugeno = rootgrp.createDimension('gaugeno', len(gaugenos))
+        iqoi = rootgrp.createDimension('iqoi', len(iqois))
+        ievent = rootgrp.createDimension('ievent', len(ievents))
+
+        gaugeno = rootgrp.createVariable('gaugeno','i4',('gaugeno',))
+        gaugeno[:] = gaugenos
+
+        ievent = rootgrp.createVariable('ievent','i4',('ievent',))
+        ievent[:] = ievents
+
+        iqoi = rootgrp.createVariable('iqoi','i4',('iqoi',))
+        iqoi[:] = iqois
+
+        x = rootgrp.createVariable('x','f8',('gaugeno',))
+        x[:] = xg
+        x.units = "longitude_east"
+
+        y = rootgrp.createVariable('y','f8',('gaugeno',))
+        y[:] = yg
+        y.units = "latitude_north"
+
+        times = rootgrp.createVariable('time',datatype,('time',))
+        times[:] = tg
+        times.units = "seconds"
+
+        gauge_vals = rootgrp.createVariable('gauge_vals',datatype,
+                            ('time','gaugeno','iqoi','ievent'))
+        gauge_vals[:,:,:,:] = gauge_results
+
+    print('Created %s' % nc_file)
+
+
+
+def read_allgauges_nc(ncfile):
+
+    import xarray
+
+    with xarray.open_dataset(ncfile, decode_timedelta=False) as ncdata:
+        print(ncdata.description)
+        gaugeno = ncdata.variables['gaugeno']
+        x = ncdata.variables['x']
+        y = ncdata.variables['y']
+        time = ncdata.variables['time']
+        ievent = ncdata.variables['ievent']
+        iqoi = ncdata.variables['iqoi']
+        gauge_vals = ncdata.variables['gauge_vals']
+
+        # create xarrays to pass back:
+        dims = ('time','gaugeno','iqoi','ievent')
+        coords = (time, gaugeno, iqoi, ievent)
+        gauge_vals = xarray.DataArray(gauge_vals, coords, dims)
+
+        gauge_x = xarray.DataArray(x, (gaugeno,), ('gaugeno',))
+        gauge_y = xarray.DataArray(y, (gaugeno,), ('gaugeno',))
+        gauge_t = asarray(time)  # simple numpy array
+
+    if 1:
+        print('Loaded %i times from t = %.1f to %.1f sec at %i gauges' \
+                % (len(time),time[0],time[-1],len(x)))
+
+    return gauge_x, gauge_y, gauge_t, gauge_vals
