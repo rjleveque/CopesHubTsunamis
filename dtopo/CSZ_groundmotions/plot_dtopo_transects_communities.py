5b7893f2785e0da4f10240b3067da56ca0f43e86
Randy LeVeque <rjl@uw.edu>
2025-09-05 07:35:10 -0700

    refactor plot_dtopo_transects_communities.py to plot for all rupture models

--- dtopo/CSZ_groundmotions/plot_dtopo_transects_communities.py
+++ dtopo/CSZ_groundmotions/plot_dtopo_transects_communities.py
@@ -1,6 +1,9 @@
 
 """
 Plot transects of dtopo deformations at final time
+
+Refactored to loop over all rupture models, and T-shirt reference events,
+reading in each dtopo file only once.
 """
 
 from pylab import *
@@ -18,96 +21,100 @@ transects = [
     (40.80, [(-124.24, 'Eureka')]),
 ]
 
-for k,transect in enumerate(transects):
-    y0 = transect[0]
+plotdir = 'seismic_transect_plots'
+os.system('mkdir -p %s' % plotdir)
 
 
-    #fname = rupture + '_ASCESIFT_y%s.png' % str(y0).replace('.','-')
-    #fname = rupture + '_Wang_y%s.png' % str(y0).replace('.','-')
+# reference events to appear on all plots:
 
-    figure(205+k,figsize=(13,6))
-    clf()
+events = ['CSZ_M1', 'CSZ_M2']
+ref_dtopos = []
+linecolors = ['k','m','c']
+for ke,event in enumerate(events):
+    fname_dtopo = '../CSZ_Tshirts/' + event + '_noext.tt3'
+    print('Reading ',fname_dtopo)
+    dtopo = dtopotools.DTopography(fname_dtopo, 3)
+    ref_dtopos.append((dtopo,event,linecolors[ke],'--'))
 
-    if 1:
-        events = []
-        #depths = ['L1','L2','L3']
-        depths = ['L1']
-        rupture = 'CSZ_'
-        for depth in depths:
-            events.append('%s%s_noext' % (rupture,depth))
+if 1:
 
-        linecolors = ['k','m','c']
-        labels = ['%s%s' % (rupture,dd) for dd in depths]
+    all_models = \
+        ['ft-locking-mur13', 'ft-locking-skl16', 'ft-locking-str10',
+         'ft-random-mur13',  'ft-random-skl16',  'ft-random-str10']
 
-        for k,event in enumerate(events):
-            print('Rupture name: ',event)
+    FrontalThrust = True
 
-            fname_dtopo = '../CSZ_Tshirts/' + event + '.tt3'
-            print('Reading ',fname_dtopo)
-            dtopo = dtopotools.DTopography(fname_dtopo, 3)
-
-            j = where(dtopo.y<y0)[0].max()
-            plot(dtopo.x,dtopo.dZ[-1,j,:],color=linecolors[k],
-                 linestyle='-.', label=labels[k])
-
-    events = []
-    depths = ['deep','middle','shallow']
-    if 0:
-        # original:
+    if not FrontalThrust:
+        all_models = [s.replace('ft','buried') for s in all_models]
         dtopo_dir = 'dtopofiles'
-        rupture = 'buried-locking-str10'
-        #rupture = 'buried-locking-mur13'
-        for depth in depths:
-            events.append('%s-%s_instant' % (rupture,depth))
-        plotdir = 'audrey_250813_nosub/transect_plots_nosub' # to compare to new ones
+    else:
+        dtopo_dir = 'dtopofiles_FrontalThrust'
 
-    if 1:
-        # poly3d applied to subsampled slip (with subevents):
-        dtopo_dir = 'dtopofiles_poly3d_5km'
-        rupture = 'buried_locking_str10'
-        #rupture = 'buried_locking_mur13'
-        for depth in depths:
-            events.append('%s_%s_poly3d_5km_instant' % (rupture,depth))
-        plotdir = 'transect_plots_poly3d_5km'
+    ruptures = all_models
+    ruptures = all_models[:1]
 
-    os.system('mkdir -p %s' % plotdir)
+    ruptures.sort()
 
-    events = events[:3]
-    linecolors = ['r','b','g']
-    #labels = depths
-    labels = ['%s-%s' % (rupture,depth) for depth in depths]
+for kr,rupture in enumerate(ruptures):
+    print('Rupture name: ',rupture)
 
-    for k,event in enumerate(events):
-        print('Rupture name: ',event)
+    events = ['%s-deep' % rupture,
+              '%s-middle' % rupture,
+              '%s-shallow' % rupture]
 
-        # Read dtopo file used for GeoClaw:
-        fname_dtopo = '%s/%s.dtt3' % (dtopo_dir, event)
-        print('Reading ',fname_dtopo)
-        dtopo = dtopotools.DTopography(fname_dtopo, 3)
+    linecolors = ['k','m','c']  # one for each depth
 
-        j = where(dtopo.y<y0)[0].max()
-        plot(dtopo.x,dtopo.dZ[-1,j,:],linecolors[k],label=labels[k])
+    # loop over transects to set up plots:
+    for kt,transect in enumerate(transects):
+        y0 = transect[0]
+        figure(205+kt,figsize=(13,6))
+        clf()
+        grid(True)
+        dzmin = -6
+        dzmax = 16
+        xlim(-127,-122)
+        ylim(dzmin, dzmax)
+        xlabel('Longitude')
+        ylabel('Vertical deformation dz (m)')
+
         title('%s\nTransect of final vertical displacement at y = %.2f' \
                 % (rupture,y0),fontsize=15)
 
-    grid(True)
-    dzmin = -6
-    dzmax = 16
-    xlim(-127,-122)
-    ylim(dzmin, dzmax)
-    xlabel('Longitude')
-    ylabel('Vertical deformation dz (m)')
-
-    for kt,clon in enumerate(transect[1]):
-        plot([clon[0],clon[0]], [dzmin,dzmax], 'g--',linewidth=1.5)
-        text(clon[0], 10.5-kt*5, 'Coast at \n%s' % clon[1], ha='center',
-             color='g',fontsize=12,backgroundcolor='w')
+        # add dashed lines and labels for coastal location(s):
+        for k,clon in enumerate(transect[1]):
+            plot([clon[0],clon[0]], [dzmin,dzmax], 'g--',linewidth=1.5)
+            text(clon[0], 10.5-k*5, 'Coast at \n%s' % clon[1], ha='center',
+                 color='g',fontsize=12,backgroundcolor='w')
+        # plot any reference dtopo's:
+        for dtopo,label,linecolor,linestyle in ref_dtopos:
+            j = where(dtopo.y<y0)[0].max()
+            plot(dtopo.x,dtopo.dZ[-1,j,:],color=linecolor,
+                 linestyle=linestyle,label=label)
 
 
+    # loop over 3 events for this rupture:
+    linecolors = ['r','b','g']
+    for ke,event in enumerate(events):
+        # Read dtopo file used for GeoClaw
+        # (use instant version since we only plot final dz, loads faster)
+        fname_dtopo = '%s/%s_instant.dtt3' % (dtopo_dir, event)
+        print('Reading ',fname_dtopo)
+        dtopo = dtopotools.DTopography(fname_dtopo, 3)
 
-    legend(loc='upper right',fontsize=10,framealpha=1)
+        # loop over trasect plots and add curve for this event:
+        for kt,transect in enumerate(transects):
+            y0 = transect[0]
+            figure(205+kt)
+            j = where(dtopo.y<y0)[0].max()
+            plot(dtopo.x,dtopo.dZ[-1,j,:],linecolors[ke],label=event)
 
-    if 1:
-        fname = '%s/dtopo_transects_%s_y%.0f.png' % (plotdir,rupture,100*y0)
-        savefig(fname)
-        print('Created ',fname)
+    # loop over transects to add legends, finalize, and save:
+    for kt,transect in enumerate(transects):
+        y0 = transect[0]
+        figure(205+kt)
+        legend(loc='upper right',fontsize=10,framealpha=1)
+
+        if 1:
+            fname = '%s/dtopo_transects_%s_y%.0f.png' % (plotdir,rupture,100*y0)
+            savefig(fname)
+            print('Created ',fname)
